13a59755ceeaf93973c38845e241d826d77764e5
Code Makers <codemakers.kr@gmail.com>
2026-03-05 12:52:49 +0900

    Refactor TableDao to use DataAccessHelper instance

--- src/main/java/kr/codemakers/oracle/tool/dao/TableDao.java
+++ src/main/java/kr/codemakers/oracle/tool/dao/TableDao.java
@@ -11,64 +11,58 @@ import kr.codemakers.oracle.tool.entity.vo.Table;
 
 public class TableDao {
 
-	private String url;
-	private int port;
-	private String database;
-	private String username;
-	private String password;
+	private DataAccessHelper helper;
 	
 	public TableDao(String url, int port, String database, String username, String password) {
-		this.url = url;
-		this.port = port;
-		this.database = database;
-		this.username = username;
-		this.password = password;
+		this.helper = new DataAccessHelper(url, port, database, username, password);
 	}
 
+	public void reconnect(String url, int port, String database, String username, String password) {
+		this.helper = new DataAccessHelper(url, port, database, username, password);
+	}
+	
+	public void close() {
+		this.helper.close();
+	}
+	
 	public List<Table> getAllTables(String username) {
-		DataAccessHelper helper = new DataAccessHelper(this.url, this.port, this.database, this.username, this.password);
+		
 		
 		List<Table> tableList = new ArrayList<>();
-		helper.preparedStatement(Querys.ALL_TABS, pstmt -> {
+		this.helper.preparedStatement(Querys.ALL_TABS, pstmt -> {
 			pstmt.setString(1, username);
 		});
-		helper.executeQuery(SQLType.SELECT, row -> {
+		this.helper.executeQuery(SQLType.SELECT, row -> {
 			String tableName = row.getString("TABLE_NAME");
 			String comments = row.getString("COMMENTS");
 			tableList.add(new Table(tableName, comments));
 		});
-		helper.close();
 		
 		return tableList;
 	}
 	
 	public Table getOneTable(String username, String tableName) {
-		DataAccessHelper helper = new DataAccessHelper(this.url, this.port, this.database, this.username, this.password);
-		
 		Table table = new Table();
-		helper.preparedStatement(Querys.ONE_TAB, pstmt -> {
+		this.helper.preparedStatement(Querys.ONE_TAB, pstmt -> {
 			pstmt.setString(1, username);
 			pstmt.setString(2, tableName);
 		});
-		helper.executeQuery(SQLType.SELECT, row -> {
+		this.helper.executeQuery(SQLType.SELECT, row -> {
 			table.setTableName(row.getString("TABLE_NAME"));
 			table.setComments(row.getString("COMMENTS"));
 		});
-		
-		helper.close();
 		
 		return table;
 	}
 	
 	public List<Column> getAllColumns(String username, String tableName) {
-		DataAccessHelper helper = new DataAccessHelper(this.url, this.port, this.database, this.username, this.password);
 		
 		List<Column> columnList = new ArrayList<>();
-		helper.preparedStatement(Querys.ALL_COLS, pstmt -> {
+		this.helper.preparedStatement(Querys.ALL_COLS, pstmt -> {
 			pstmt.setString(1, username);
 			pstmt.setString(2, tableName);
 		});
-		helper.executeQuery(SQLType.SELECT, row -> {
+		this.helper.executeQuery(SQLType.SELECT, row -> {
 			String columnName = row.getString("COLUMN_NAME");
 			String dataType = row.getString("DATA_TYPE");
 			String comments = row.getString("COMMENTS");
@@ -76,7 +70,6 @@ public class TableDao {
 			columnList.add(new Column(columnName, dataType, comments, length));
 		});
 		
-		helper.close();
 		return columnList;
 	}
 }
